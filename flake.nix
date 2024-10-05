de8c6da85e23c57428ac98b54f29edf0b82ab1c3
TacoConKvass <e_frun@o2.pl>
2024-10-05 21:28:29 +0000

    [Addition] - WSL configuration

--- flake.nix
+++ flake.nix
@@ -4,15 +4,27 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 		nixvim.url = "github:nix-community/nixvim/nixos-24.05";
+		nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 	};
 
 	outputs = { nixpkgs, ... } @ inputs: {
-
 		nixosConfigurations.taco-bell = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
 				./hosts/taco-bell
-        inputs.nixvim.nixosModules.nixvim
+				inputs.nixvim.nixosModules.nixvim
+			];
+		};
+		
+		nixosConfigurations.taco-wsl = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./hosts/wsl
+				inputs.nixvim.nixosModules.nixvim
+				inputs.nixos-wsl.nixosModules.default {
+	  				system.stateVersion = "24.05";
+					wsl.enable = true;
+	    			}
 			];
 		};
 	};
---
+++ hosts/wsl/default.nix
@@ -0,0 +1,52 @@
+{ ... }:
+{
+        imports = {
+ 		../../users
+		../../modules
+        ];
+
+        # Enable networking
+        networking.hostName = "taco-wsl";
+        networking.networkmanager.enable = true;
+
+        # Enable the X11 windowing system.
+        # Enable the KDE Plasma Desktop Environment.
+        services.xserver.enable = true;
+        services.displayManager.sddm.enable = true;
+        services.desktopManager.plasma6.enable = true;
+
+        # Configure keymap in X11
+        services.xserver.xkb = {
+                layout = "pl";
+                variant = "";
+        };
+
+        # Enable sound with pipewire.
+        hardware.pulseaudio.enable = false;
+        security.rtkit.enable = true;
+        services.pipewire = {
+                enable = true;
+                alsa.enable = true;
+                alsa.support32Bit = true;
+                pulse.enable = true;
+        };
+
+        # Enable flakes
+        nix.settings.experimental-features = ["nix-command" "flakes"];
+
+        programs = {
+                firefox.enable = true;
+        };
+
+	modules = {
+		nixvim.enable = true;
+		internalisation.enable = true;
+	};
+
+        # Allow unfree packages
+        nixpkgs.config.allowUnfree = true;
+
+        # NixOS version
+        system.stateVersion = "24.05";
+
+}
