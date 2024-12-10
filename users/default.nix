a56700274794601081b4059b6e36d99c11a460bd
TacoConKvass <e_frun@o2.pl>
2024-12-10 11:03:30 +0100

    [Remove] - Dev packages. All repositiories will have a nix shell from now on

--- users/default.nix
+++ users/default.nix
@@ -1,24 +1,12 @@
 { pkgs, ... }:
 {
-	nixpkgs = {
-    config = {
-    	allowUnfree = true;
-    	packageOverrides = pkgs: {
-    		unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
-    	};
-  	};
-	};
-
 	users.users.taco = {
     isNormalUser = true;
     description = "Taco";
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
-      comma
       fastfetch
       git
-			dotnet-sdk_8
-			unstable.zig
 		];
   };
 
