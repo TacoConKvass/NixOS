ee49c8c824a72a0c94691094d4d5109363e22c60
TacoConKvass <e_frun@o2.pl>
2025-08-04 11:50:33 +0000

    [Update] Git module

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,54 +1,75 @@
 { config, lib, pkgs, ... } : let
     cfg = config.modules.git;
-    pkgsAttr = if (builtins.hasAttr "packages" config.environment) then "packages" else "systemPackages";
     gcm = "git-credential-manager";
+    isAndroid = builtins.pathExists /storage/emulated;
+    at = index: list: builtins.elemAt list index;
+    pkgsAttr = if (isAndroid) then "packages" else "systemPackages";
+    scriptAttr = lib.splitString "." (if (isAndroid) then "build.activationAfter" else "system.activationScripts");
 in {
     options.modules.git = {
         enable = lib.mkEnableOption "Enable Git";
-        username = lib.mkOption {
-            type = lib.types.str;
-            description = "Username used for Git";
-        };
-        email = lib.mkOption {
-            type = lib.types.str;
-            description = "Email used for Git";
-        };
-        gcm = {
-            enable = lib.mkEnableOption "Enable Git Credential Manager";
-            store = lib.mkOption {
-                type = lib.types.enum ["cache" "plaintext" "gpg"];
-                description = ''What storing mechanism to use
-                    "cache" - timed cache, keeps credentials for 15 minutes
-                    "plaintext" - keeps credentials in plaintext - DO NOT USE UNLESS NO OTHER OPTION IS AVAILABLE
-                    "gpg" - GPG/pass compatible files
-
-                    Information taken from: https://github.com/git-ecosystem/git-credential-manager/blob/main/docs/credstores.md
-                '';
-            };
-            cacheOptions = lib.mkOption {
+        config = {
+            enable = lib.mkEnableOption "Setup .gitconfig";
+            username = lib.mkOption {
                 type = lib.types.str;
-                default = "";
-                description = ''Credentials cache options. Only take effect if gcm.store is set to "cache"'';
+                description = "Username used for Git";
             };
+            email = lib.mkOption {
+                type = lib.types.str;
+                description = "Email used for Git";
+            };
+            gcm = {
+                enable = lib.mkEnableOption "Enable Git Credential Manager";
+                storeType = lib.mkOption {
+                    type = lib.types.enum ["cache" "plaintext" "gpg"];
+                    description = ''What storing mechanism to use
+                        "cache" - timed cache, keeps credentials for 15 minutes
+                        "plaintext" - keeps credentials in plaintext - DO NOT USE UNLESS NO OTHER OPTION IS AVAILABLE
+                        "gpg" - GPG/pass compatible files
+
+                        Information taken from: https://github.com/git-ecosystem/git-credential-manager/blob/main/docs/credstores.md
+                    '';
+                };
+                cacheOptions = lib.mkOption {
+                    type = lib.types.str;
+                    default = "";
+                    description = ''Credentials cache options. Only take effect if gcm.storeType is set to "cache"'';
+                };
+            };
+            home = lib.mkOption {
+                type = lib.types.path;
+                description = "Home path to put .gitconfig into";
+            };
+        };
+        package = lib.mkOption {
+            type = lib.types.package;
+            default = pkgs.git;
+            description = "Git package";
         };
     };
 
-    config = lib.mkIf cfg.enable {
-        environment.${pkgsAttr} = lib.mkIf cfg.gcm.enable ([ pkgs.${gcm} ]
-            ++ lib.optionals (cfg.gcm.store == "gpg") [ pkgs.pass ]
+    config = lib.mkIf (cfg.enable) {
+        environment.${pkgsAttr} = ([ cfg.package ]
+            ++ (lib.optionals cfg.config.gcm.enable ([ pkgs.${gcm} ]
+                ++ lib.optionals (cfg.config.gcm.storeType == "gpg") [ pkgs.pass ]
+            ))
         );
 
-        programs.git = lib.mkIf cfg.enable {
-            enable = true;
-            config = {
-                credential = lib.mkIf cfg.gcm.enable {
-                    helper = "${pkgs.${gcm}}/bin/${gcm}";
-                    credentialStore = cfg.gcm.store;
-                    cacheOptions = lib.mkIf (cfg.gcm.store == "cache") (cfg.gcm.cacheOptions);
-                };
-                user.name = cfg.username;
-                user.email = cfg.email;
-            };
-        };
+        ${at 0 scriptAttr}.${at 1 scriptAttr}.setupGit = ((lib.mkIf cfg.config.enable) ''
+            if [ ! -d ${cfg.config.home}/gitconfig ]; then
+                echo "Creating Git config in ${cfg.config.home}..."
+                echo '
+            [user]
+                name = "${cfg.config.username}"
+                email = "${cfg.config.email}"
+            [credential]
+                helper = ${"${pkgs.${gcm}}/bin/${gcm}"}
+                credentialStore = "${cfg.config.gcm.storeType}"
+                cacheOptions = "${cfg.config.gcm.cacheOptions}"
+            ' > ${cfg.config.home}/gitconfig
+            else
+                echo "Git config found..."
+            fi
+        '');
     };
 }
