46d689ccf49e4f48e90b1227521d4e1c2a6fea66
TacoConKvass <e_frun@o2.pl>
2025-08-05 07:43:33 +0000

    [Add] csharp added to the module, add LSP packages

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -5,12 +5,19 @@ in {
     options.modules.dev = {
         zig = lib.mkEnableOption "Ensure the Zig compiler is installed";
         rust = lib.mkEnableOption "Ensure tools for Rust development are installed";
+        cSharp = lib.mkEnableOption "Ensure tools for C# development are installed";
     };
 
     config = {
         environment.${pkgsAttr} = ([]
             ++ (lib.optionals cfg.zig [ pkgs.zig ])
             ++ (lib.optionals cfg.rust [ pkgs.cargo pkgs.rustc pkgs.gcc ])
+            ++ (lib.optionals cfg.cSharp [ pkgs.dotnetCorePackages.dotnet_8.sdk ])
         );
+
+        modules.neovim.additionalPackages = []
+            ++ (lib.optionals cfg.zig [ pkgs.zls ])
+            ++ (lib.optionals cfg.rust [ pkgs.rust-analyzer ])
+            ++ (lib.optionals cfg.cSharp [ pkgs.roslyn-ls ]);
     };
 }
