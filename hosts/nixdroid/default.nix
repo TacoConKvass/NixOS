84a4b9088db8f356648efebe7218b220ffcf10bd
TacoConKvass <e_frun@o2.pl>
2025-09-11 09:43:28 +0000

    [Change] Customization for modules.dev.zig

--- hosts/nixdroid/default.nix
+++ hosts/nixdroid/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... } : let
+{ config, pkgs, unstable, ... } : let
     user = config.user;
 in {
     imports = [
@@ -7,7 +7,11 @@ in {
     ];
 
     modules = {
-        dev.zig = true;
+        dev.zig = {
+            enable = true;
+            package = unstable.zig_0_15;
+            languageServer = unstable.zls_0_15;
+        };
         dev.rust = true;
 
         dotfiles = {
--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -3,14 +3,26 @@
     dotnetPkgs = pkgs.dotnetCorePackages;
 in {
     options.modules.dev = {
-        zig = lib.mkEnableOption "Ensure the Zig compiler is installed";
+        zig =  {
+            enable = lib.mkEnableOption "Ensure the Zig compiler is installed";
+            package = lib.mkOption {
+                type = lib.types.package;
+                default = pkgs.zig;
+                description = "Package to pull as the Zig compiler";
+            };
+            languageServer = lib.mkOption {
+                type = lib.types.package;
+                default = pkgs.zls;
+                description = "Package to pull as the Zig language server";
+            };
+        };
         rust = lib.mkEnableOption "Ensure tools for Rust development are installed";
         cSharp = lib.mkEnableOption "Ensure tools for C# development are installed";
     };
 
     config = {
         environment.systemPackages = ([]
-            ++ (lib.optionals cfg.zig [ pkgs.zig ])
+            ++ (lib.optionals cfg.zig.enable [ cfg.zig.package ])
             ++ (lib.optionals cfg.rust [ pkgs.cargo pkgs.rustc pkgs.gcc ])
             ++ (lib.optionals cfg.cSharp [(
                 dotnetPkgs.combinePackages [
@@ -22,7 +34,7 @@ in {
         );
 
         modules.neovim.additionalPackages = []
-            ++ (lib.optionals cfg.zig [ pkgs.zls ])
+            ++ (lib.optionals cfg.zig.enable [ cfg.zig.languageServer])
             ++ (lib.optionals cfg.rust [ pkgs.rust-analyzer ])
             ++ (lib.optionals cfg.cSharp [ pkgs.omnisharp-roslyn ]);
     };
