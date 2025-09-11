028c07f4b88f2a1d8a4467ede3a1c252f38244c9
TacoConKvass <e_frun@o2.pl>
2025-09-11 09:42:58 +0000

    [Add] Support for using unstable packages

--- flake.nix
+++ flake.nix
@@ -7,23 +7,25 @@
             url = "github:nix-community/nix-on-droid/release-24.05";
             inputs.nixpkgs.follows = "nixdroid-pkgs";
         };
-        
+
         pkgs-25-05.url = "github:NixOS/nixpkgs/nixos-25.05";
-    
+        pkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
         wsl.url = "github:nix-community/NixOS-WSL/2411.6.0";
     };
 
-    outputs = { ... } @ inputs : 
-        let
-            arm = "aarch64-linux";
-            x86 = "x86_64-linux";
-        in
-    {
+    outputs = { ... } @ inputs : let
+        arm = "aarch64-linux";
+        x86 = "x86_64-linux";
+    in {
         nixOnDroidConfigurations.default = inputs.nixdroid.lib.nixOnDroidConfiguration {
             pkgs = import inputs.pkgs-25-05 { system = arm; };
             modules = [
                 ./hosts/nixdroid
             ];
+            extraSpecialArgs = {
+                unstable = inputs.pkgs-unstable.legacyPackages.${arm};
+            };
         };
 
         nixosConfigurations.wsl = inputs.pkgs-25-05.lib.nixosSystem {
