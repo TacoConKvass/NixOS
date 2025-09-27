677258c2707f407aa2ec8db9ce4720a6af636f23
TacoConKvass <e_frun@o2.pl>
2025-09-27 17:07:49 +0000

    [Add] Zig 0.15 for WSL

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,10 +1,14 @@
-{ config, ... } : let
+{ config, unstable, ... } : let
     user = config.users.users.nixos;
 in {
     imports = [ ./../../modules ];
 
     modules = {
-        dev.zig = true;
+        dev.zig = {
+            enable = true;
+            package = unstable.zig_0_15;
+            languageServer = unstable.zls_0_15;
+        };
         dev.rust = true;
         dev.cSharp = true;
 
