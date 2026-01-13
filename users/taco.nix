292839c6e2bbcced5a3786ce283cc1e6e579c49b
TacoConKvass <e_frun@o2.pl>
2026-01-13 22:15:04 +0100

    Remove: Unused/Useless configs from taco

--- users/taco.nix
+++ users/taco.nix
@@ -11,19 +11,6 @@ in {
         group = "users";
         home = "/home/taco";
         shell = pkgs.bash;
-        packages = [ pkgs.nerd-fonts.jetbrains-mono ];
-    };
-
-    modules.neovim = {
-        enable = true;
-        config = {
-            pull = true;
-            repository = {
-                url = "https://github.com/TacoConKvass/nvim";
-                branch = "lazy";
-            };
-            user = taco;
-        };
     };
 
     modules.git = {
@@ -42,13 +29,10 @@ in {
 
     files = let cfg = "${home}/.config"; in {
         "${home}/.bashrc".source = ./../dotfiles/bash;
-        "${cfg}/ghostty/config".source = ./../dotfiles/ghostty;
         "${cfg}/niri/config.kdl".source = ./../dotfiles/niri.kdl;
         "${cfg}/waybar/style.css".source = ./../dotfiles/waybar.css;
         "${cfg}/waybar/config.jsonc".source = ./../dotfiles/waybar.jsonc;
         "${cfg}/wallpaper.png".source = ./../dotfiles/wallpaper.png;
         "${cfg}/fuzzel/fuzzel.ini".source = ./../dotfiles/fuzzel.ini;
-        "${cfg}/gtk-3.0/gtk.css".source = ./../dotfiles/gtk.css;
-        "${cfg}/xfce4/xfconf/xfce-perchannel-xml/thunar.xml".source = ./../dotfiles/thunar.xml;
     };
 }
